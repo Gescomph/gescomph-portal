915f52e32259060c511f7fdcd53de4aa48ce9bfa
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-10-30 01:45:55 -0500

    init

--- GESCOMPH/src/environments/environment.development.ts
+++ GESCOMPH/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   // apiURL: "https://localhost:7165/api",
-  apiURL: "http://localhost:5100/api",
+  apiURL: "http://localhost:5101/api",
 
 
   // 0 = sin límite; ajusta según despliegue
