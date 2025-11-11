fe93b63cba73f6817bd64ccba1cf8dafd918a089
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-11-11 17:39:03 -0500

    chore(env): update API base URL to develop server

--- GESCOMPH/src/environments/environment.development.ts
+++ GESCOMPH/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   // apiURL: "https://localhost:7165/api",
-  apiURL: "http://localhost:5103/api",
+  apiURL: "http://localhost:5100/api",
 
 
   // 0 = sin límite; ajusta según despliegue
