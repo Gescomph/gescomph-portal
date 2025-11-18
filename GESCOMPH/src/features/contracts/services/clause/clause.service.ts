9696bdaf7a8e8248141acd672db55c1f6f9413ab
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-11-18 10:27:28 -0500

    feat(HU-02-clauses): implement clauses management with CRUD operations and UI integration

--- GESCOMPH/src/features/contracts/services/clause/clause.service.ts
+++ GESCOMPH/src/features/contracts/services/clause/clause.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { ClauseSelect } from '../../models/clause.models';
+import { ClauseCreate, ClauseSelect, ClauseUpdate } from '../../models/clause.models';
 import { environment } from '../../../../environments/environment.development';
 
 @Injectable({ providedIn: 'root' })
@@ -13,5 +13,16 @@ export class ClauseService {
   getAll(): Observable<ClauseSelect[]> {
     return this.http.get<ClauseSelect[]>(this.baseUrl);
   }
-}
 
+  create(body: ClauseCreate): Observable<ClauseSelect> {
+    return this.http.post<ClauseSelect>(this.baseUrl, body);
+  }
+
+  update(body: ClauseUpdate): Observable<ClauseSelect> {
+    return this.http.put<ClauseSelect>(`${this.baseUrl}/${body.id}`, body);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
