267f61830ca55141a0b64dd21de29db293abab29
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-11-25 20:23:22 -0500

    feat(payment-gateway): add `payment-success` component with carousel and interactive cards, and set up initial env and routing infrastructure (HU-05)

--- GESCOMPH/src/environments/environment.development.ts
+++ GESCOMPH/src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   // apiURL: "https://localhost:7165/api",
-  apiURL: "http://localhost:5100/api",
+  apiURL: "https://gescomph-api-dev.nicemushroom-3213f79c.eastus2.azurecontainerapps.io/api",
 
 
   // 0 = sin límite; ajusta según despliegue
--- GESCOMPH/src/environments/environment.ts
+++ GESCOMPH/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // 0 = sin limite; ajusta segun despliegue
   // apiURL: "https://localhost:7165/api",
-  apiURL: "http://localhost:5100/api",
+  apiURL: "https://gescomph-api-dev.nicemushroom-3213f79c.eastus2.azurecontainerapps.io/api",
 
   establishmentsDefaultLimit: 0
 };
